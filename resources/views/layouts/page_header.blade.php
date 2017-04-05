014a9a1d3a83495f326921a0d9ba9ae14b405098
tuyennc <tuyennc@k.s-cubism.jp>
2017-04-05 17:29:09 +0700

    remove code

--- resources/views/layouts/page_header.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h3 class="page-header"><i class="@yield('icon')"></i>@yield('page_header')</h3>
-        </div>
-    </div>
-</div>
